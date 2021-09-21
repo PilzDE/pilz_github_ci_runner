c7cae0610c1bbf426055fb84b763551ea5e86fb5
Immanuel Martini <i.martini@pilz.de>
2021-09-21 14:23:33 +0200

    Add missing import of time module

--- src/pilz_github_ci_runner/pr_check_executor.py
+++ src/pilz_github_ci_runner/pr_check_executor.py
@@ -14,6 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import github
+import time
 from collections import namedtuple
 from typing import Sequence
 from github.GithubException import RateLimitExceededException, GithubException
